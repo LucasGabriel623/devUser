1a6bc3b1eaeb9f3ae2a838b6f3891c71ea6a77c3
LucasGabriel623 <gabrieloliveira.uru@gmail.com>
2021-01-17 19:32:08 -0300

    atualizacao projeto

---
+++ core/Config.php
@@ -0,0 +1,15 @@
+<?php
+session_start();
+ob_start();
+
+define('URL', 'https://projetosdev.info/usuarios/');
+define('URLADM', 'https://projetosdev.info/usuarios/');
+
+define('CONTROLER', 'Home');
+define('METODO', 'index');
+
+//Credenciais de acesso ao BD
+define('HOST', 'localhost');
+define('USER', 'u224083565_devusuario');
+define('PASS', 'Dev#1449#');
+define('DBNAME', 'u224083565_usuarios');
